d2b66cd7066aafab7cd8940bed43e53e8c732d0c
Sathya priya S <sathyapriya0824@gmail.com>
2026-02-10 10:39:40 +0530

    Add files via upload

---
+++ MCQ App.py
@@ -0,0 +1,126 @@
+
+score = 0
+print("BIOLOGY MCQ QUIZ (20 QUESTIONS)\n")
+
+print("1. Cell is the basic unit of?")
+print("a) Tissue b) Organ c) Life d) System")
+ans = input("Enter answer: ")
+if ans.lower() == "c":
+    score += 1
+
+print("\n2. Human heart has how many chambers?")
+print("a) 2 b) 3 c) 4 d) 5")
+ans = input("Enter answer: ")
+if ans.lower() == "c":
+    score += 1
+
+print("\n3. Photosynthesis occurs in?")
+print("a) Mitochondria b) Chloroplast c) Nucleus d) Ribosome")
+ans = input("Enter answer: ")
+if ans.lower() == "b":
+    score += 1
+
+print("\n4. Largest organ in human body?")
+print("a) Heart b) Brain c) Skin d) Liver")
+ans = input("Enter answer: ")
+if ans.lower() == "c":
+    score += 1
+
+print("\n5. Blood is a type of?")
+print("a) Muscle b) Tissue c) Organ d) Cell")
+ans = input("Enter answer: ")
+if ans.lower() == "b":
+    score += 1
+
+print("\n6. Which vitamin is produced in skin?")
+print("a) A b) B c) C d) D")
+ans = input("Enter answer: ")
+if ans.lower() == "d":
+    score += 1
+
+print("\n7. Which gas is released during photosynthesis?")
+print("a) Oxygen b) Carbon dioxide c) Nitrogen d) Hydrogen")
+ans = input("Enter answer: ")
+if ans.lower() == "a":
+    score += 1
+
+print("\n8. Plant cells have?")
+print("a) Cell wall b) Centriole c) Lysosome d) Flagella")
+ans = input("Enter answer: ")
+if ans.lower() == "a":
+    score += 1
+
+print("\n9. Which part controls the body?")
+print("a) Heart b) Brain c) Lungs d) Kidney")
+ans = input("Enter answer: ")
+if ans.lower() == "b":
+    score += 1
+
+print("\n10. DNA is present in?")
+print("a) Cytoplasm b) Ribosome c) Nucleus d) Vacuole")
+ans = input("Enter answer: ")
+if ans.lower() == "c":
+    score += 1
+
+print("\n11. Which organ helps in breathing?")
+print("a) Heart b) Lung c) Kidney d) Liver")
+ans = input("Enter answer: ")
+if ans.lower() == "b":
+    score += 1
+
+print("\n12. Smallest bone in human body?")
+print("a) Femur b) Stapes c) Radius d) Ulna")
+ans = input("Enter answer: ")
+if ans.lower() == "b":
+    score += 1
+
+print("\n13. Which blood group is universal donor?")
+print("a) A b) B c) AB d) O")
+ans = input("Enter answer: ")
+if ans.lower() == "d":
+    score += 1
+
+print("\n14. Which organ purifies blood?")
+print("a) Liver b) Lung c) Kidney d) Heart")
+ans = input("Enter answer: ")
+if ans.lower() == "c":
+    score += 1
+
+print("\n15. Plant food is prepared in?")
+print("a) Root b) Stem c) Leaf d) Flower")
+ans = input("Enter answer: ")
+if ans.lower() == "c":
+    score += 1
+
+print("\n16. Which is the largest bone?")
+print("a) Femur b) Skull c) Spine d) Rib")
+ans = input("Enter answer: ")
+if ans.lower() == "a":
+    score += 1
+
+print("\n17. Which organ stores urine?")
+print("a) Kidney b) Bladder c) Liver d) Lung")
+ans = input("Enter answer: ")
+if ans.lower() == "b":
+    score += 1
+
+print("\n18. Which vitamin helps blood clotting?")
+print("a) A b) B c) K d) D")
+ans = input("Enter answer: ")
+if ans.lower() == "c":
+    score += 1
+
+print("\n19. Which cell has no nucleus?")
+print("a) RBC b) WBC c) Nerve d) Muscle")
+ans = input("Enter answer: ")
+if ans.lower() == "a":
+    score += 1
+
+print("\n20. Which system controls hormones?")
+print("a) Digestive b) Nervous c) Endocrine d) Circulatory")
+ans = input("Enter answer: ")
+if ans.lower() == "c":
+    score += 1
+
+print("\nQuiz Completed!")
+print("Your Total Score is:", score, "/ 20")
